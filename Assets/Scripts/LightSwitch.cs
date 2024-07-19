7ef99d298d2a77fd600c6467911f8e8e169c791e
Anthony Lacasse <belowaveragedreamer@gmail.com>
2024-07-18 22:53:39 -0400

    LightSwitches
    Script pour les lightswitches et gym

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour, Interactable
+{
+    [SerializeField] private List<Light> m_Lights;
+
+    private Color initialColor;
+
+    void Start()
+    {
+        initialColor = GetComponent<Renderer>().material.color;
+    }
+
+    public void InRange(bool inRange)
+    {
+        if (inRange)
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+
+        }
+        else //off
+        {
+            GetComponent<Renderer>().material.color = initialColor;
+        }
+    }
+
+    public void Use()
+    {
+        foreach (Light light in m_Lights)
+        {
+            light.enabled = !light.enabled;
+        }
+    }
+
+    InteractibleType Interactable.GetType()
+    {
+       return InteractibleType.LIGHT;
+    }
+
+}
